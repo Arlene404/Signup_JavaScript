0de58fb9b1d7b834ed0af168855f91e29737a825
Arlene404 <giraarle080@gmail.com>
2022-10-18 16:47:22 +0200

    Signup page using Javascript

---
+++ DOM.js
@@ -0,0 +1,57 @@
+// var h1 = document.getElementsByTagName("h1");
+// console.log(h1);
+// paragraphs[0].innerHTML="WOWWW";
+// paragraphs[0].innerHTML="<h1>This is coming from Js</h1>";
+
+// paragraphs[1].textContents="<h1>This is coming from Js</h1>";
+
+// var content = document.getElementsByClassName("content");
+// var count = document.getElementById("one");
+
+// box1.style.color="red";
+
+// var containers = document.querySelectorAll(".content");
+// containers[1].style.color="#dddd";
+
+// content.array.forEach(element => {
+    
+// });
+
+// content.forEach(function(currentItem){
+//     currentItem.style.color="#aaaa";
+// })
+
+
+// for (let index=0; index<SVGTextContentElement.length; index++){
+//     content[index].style.color="#fffff"
+// }
+// console.log(box1);
+
+// function Subscribe(){
+//     var output = document.getElementById("output");
+//     output.innerHTML="Welcome" + "" + username.value;
+//     // alert("Button Clicked");
+
+// }
+
+
+// const form = document.getElementById('form');
+
+
+
+function logIn(){
+var name = document.getElementById("name");
+var email = document.getElementById("email");
+var putout = document.getElementById("putout");
+var output = document.getElementById("output");
+
+putout.innerHTML = "Please enter your name" + name.value;
+output.innerHTML = "Please enter your email" + email.value;
+
+}
+
+
+
+
+
+
